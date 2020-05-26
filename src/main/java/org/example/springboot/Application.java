76423cb4698e07304cdf3fb33d0c313ff69873a6
coldflow <dev.coldflow@gmail.com>
2020-05-27 03:22:14 +0900

    [UPDATE] API 만드는 중(성공 저장)

---
+++ src/main/java/org/example/springboot/Application.java
@@ -0,0 +1,17 @@
+
+/*
+이 Application class는 이 포로젝트의 메인 클래스이다.
+@springbootapplication으로 인해 스프링 부트의 자동설정, 스프링bean 읽기와 생성을 모두 자동으로 설정한다.
+특히나 @springbootapplication이 있는 위치부터 설정을 읽어가기 때문에 이 프로젝트는 항상 최상단에 위치해야 한다.
+ */
+package org.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args); // >> 내장 WAS를 실행
+    }
+}
