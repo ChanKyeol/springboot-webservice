3098e71e04ecfed86e5ed97bdd2f451cf6fa8173
coldflow <dev.coldflow@gmail.com>
2020-07-23 14:47:46 +0900

    delete juseok

--- src/main/java/org/example/springboot/web/PostsApiController.java
+++ src/main/java/org/example/springboot/web/PostsApiController.java
@@ -28,6 +28,7 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
+    //임시 테스트용 주석(지우기)
     @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
